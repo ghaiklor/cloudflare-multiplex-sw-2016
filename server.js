2e85cd6ad8c10de63b643bb60c95b25ade1156e8
Eugene Obrezkov <ghaiklor@gmail.com>
2016-08-07 12:56:22 +0300

    sw: implement simple registration of SW

---
+++ server.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const connect = require('connect');
+const serveStatic = require('serve-static');
+
+connect().use(serveStatic(__dirname)).listen(8080, function () {
+  console.log('Server is running on 8080...');
+});
---
+++ sw.js
@@ -0,0 +1,1 @@
+importScripts('/src/vendor/serviceworker-cache-polyfill.js');
