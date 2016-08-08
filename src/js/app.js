34851559cb2b1d4067c389ba417d389fb3783309
Eugene Obrezkov <ghaiklor@gmail.com>
2016-08-08 20:45:45 +0300

    docs: add comments describing our dirty hack for fetch event

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,11 @@ navigator
   .serviceWorker
   .register('service-worker.js', {scope: '/'})
   .then(registration => {
+    // This dirty hack is needed because
+    // SW registers later than video starts playing OR because of caching
+    // Anyway, fetch event in SW is not catching video fetching
+    // So, I am replacing video source with URL and a timestamp for invalidating cache
+    // And, finally, got this working
     const videos = document.querySelectorAll('video');
 
     for (let i = 0; i < videos.length; i++) {
