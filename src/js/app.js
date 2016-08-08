28c8ed3879527aad61cbc4498c15afabcbbe3317
Eugene Obrezkov <ghaiklor@gmail.com>
2016-08-08 19:55:37 +0300

    feature: start playing videos only when SW is registered

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,13 @@
-navigator.serviceWorker.register('service-worker.js', {scope: '/'});
+"use strict";
+
+navigator
+  .serviceWorker
+  .register('service-worker.js', {scope: '/'})
+  .then(registration => {
+    const videos = document.querySelectorAll('video');
+
+    for (let i = 0; i < videos.length; i++) {
+      videos[i].src = videos[i].getAttribute('data-src');
+    }
+  })
+  .catch(() => alert('Switch to Google Chrome 52.0.2743.116 (64-bit)'));
