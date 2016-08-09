d845e5c4226374e7f1d6119d343e54effc5ce1ef
Eugene Obrezkov <ghaiklor@gmail.com>
2016-08-09 15:51:33 +0300

    refactor: replace own onRequest with serve-static

--- src/server.js
+++ src/server.js
@@ -3,22 +3,12 @@
 const fs = require('fs');
 const path = require('path');
 const http2 = require('http2');
+const serve = require('serve-static');
 
 const KEY = fs.readFileSync(path.join(__dirname, '../cert/localhost.key'));
 const CERT = fs.readFileSync(path.join(__dirname, '../cert/localhost.crt'));
 const PORT = 8080;
 
-function onRequest(request, response) {
-  const filename = request.url === '/' ? path.join(__dirname, 'index.html') : path.join(__dirname, request.url);
-
-  if ((filename.indexOf(__dirname) === 0) && fs.existsSync(filename) && fs.statSync(filename).isFile()) {
-    fs.createReadStream(filename).pipe(response).on('finish', response.end);
-  } else {
-    response.writeHead(404);
-    response.end();
-  }
-}
-
 http2
-  .createServer({key: KEY, cert: CERT}, onRequest)
+  .createServer({key: KEY, cert: CERT}, serve(__dirname))
   .listen(PORT);
