855559339fceacdb4fbee5bbc638d73e3dadf1c1
Eugene Obrezkov <ghaiklor@gmail.com>
2016-08-09 14:14:59 +0300

    feature: implement HTTP/2 support

--- src/server.js
+++ src/server.js
@@ -1,10 +1,27 @@
 "use strict";
 
-const connect = require('connect');
-const serveStatic = require('serve-static');
+const fs = require('fs');
+const path = require('path');
+const http2 = require('http2');
 
+const KEY = fs.readFileSync(path.join(__dirname, '../cert/localhost.key'));
+const CERT = fs.readFileSync(path.join(__dirname, '../cert/localhost.crt'));
 const PORT = 8080;
 
-connect()
-  .use(serveStatic(__dirname))
-  .listen(PORT, () => console.log(`Server is running on ${PORT}...`));
+function onRequest(request, response) {
+  const filename = path.join(__dirname, request.url);
+
+  if ((filename.indexOf(__dirname) === 0) && fs.existsSync(filename) && fs.statSync(filename).isFile()) {
+    response.writeHead(200);
+    const fileStream = fs.createReadStream(filename);
+    fileStream.pipe(response);
+    fileStream.on('finish', response.end);
+  } else {
+    response.writeHead(404);
+    response.end();
+  }
+}
+
+http2
+  .createServer({key: KEY, cert: CERT}, onRequest)
+  .listen(PORT);
