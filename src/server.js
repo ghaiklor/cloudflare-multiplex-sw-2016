e10ad56ffef9855a5a134dae0549d8090d4bc71e
Eugene Obrezkov <ghaiklor@gmail.com>
2016-08-16 16:48:08 +0300

    http: change back to HTTP/1.1

--- src/server.js
+++ src/server.js
@@ -1,14 +1,10 @@
 "use strict";
 
-const fs = require('fs');
-const path = require('path');
-const http2 = require('http2');
+const connect = require('connect');
 const serve = require('serve-static');
 
-const KEY = fs.readFileSync(path.join(__dirname, '../cert/localhost.key'));
-const CERT = fs.readFileSync(path.join(__dirname, '../cert/localhost.crt'));
 const PORT = 8080;
 
-http2
-  .createServer({key: KEY, cert: CERT}, serve(__dirname))
-  .listen(PORT);
+connect()
+  .use(serve(__dirname))
+  .listen(PORT, () => console.log(`Server is listening on ${PORT}...`));
