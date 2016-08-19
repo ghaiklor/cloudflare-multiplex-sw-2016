0705be370867a6365459e2a8e9b8ed8eabe537aa
Eugene Obrezkov <ghaiklor@gmail.com>
2016-08-19 15:43:11 +0300

    refactor: remove dirty hack with video data-src

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,7 @@
 "use strict";
 
-navigator
-  .serviceWorker
-  .register('service-worker.js', {scope: '/'})
-  .then(registration => {
-    // This dirty hack is needed because
-    // SW registers later than video starts playing OR because of caching
-    // Anyway, fetch event in SW is not catching video fetching
-    // So, I am replacing video source with URL and a timestamp for invalidating cache
-    // And, finally, got this working
-    const videos = document.querySelectorAll('video');
-
-    for (let i = 0; i < videos.length; i++) {
-      videos[i].src = `${videos[i].getAttribute('data-src')}?t=${Date.now()}`;
-    }
-  })
-  .catch(() => alert('Switch to Google Chrome 52.0.2743.116 (64-bit)'));
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js', {scope: '/'});
+} else {
+  console.warn(`Your browser doesn't support Service Workers`);
+}
