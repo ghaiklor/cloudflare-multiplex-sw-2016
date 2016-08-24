76f8b06db05b59cbb92872d70569d57698164796
Eugene Obrezkov <ghaiklor@gmail.com>
2016-08-24 13:04:20 +0300

    feature: implement simple copying of all requests params in new requests

--- src/service-worker.js
+++ src/service-worker.js
@@ -21,40 +21,74 @@ function concatArrayBuffer(ab1, ab2) {
 
 /**
  * Builds an array of separate requests to URL with specified ContentLength
- * @param {String} url Target URL
+ * @param {Request} request Request object from original request
  * @param {Number} length Total length of content located by URL
  * @returns {Array<Promise>} Returns an array of promises built by fetch()
  * @private
  */
-function buildPartialRequests(url, length) {
+function buildPartialRequests(request, length) {
   return Array
     .from({length: Math.ceil(length / CHUNK_SIZE)})
     .map((_, i) => {
-      return fetch(url, {
-        headers: new Headers({
-          'Range': `bytes=${i * CHUNK_SIZE}-${(i * CHUNK_SIZE) + CHUNK_SIZE - 1}/${length}`
-        })
-      })
+      //FIXME: Failed to execute 'set' on 'Headers': Headers are immutable
+      const newHeaders = request.headers;
+      newHeaders.set('Range', `bytes=${i * CHUNK_SIZE}-${(i * CHUNK_SIZE) + CHUNK_SIZE - 1}/${length}`);
+
+      const newRequest = new Request(request.url, {
+        method: request.method,
+        headers: newHeaders,
+        body: request.body,
+        mode: request.mode,
+        credentials: request.credentials,
+        cache: request.cache,
+        redirect: request.redirect,
+        referrer: request.referrer,
+        integrity: request.integrity
+      });
+
+      return fetch(newRequest);
     });
 }
 
 /**
  * Triggers each time when HEAD request is successful
- * @param {Response} response
+ * @param {FetchEvent} event Original FetchEvent from request
+ * @param {Response} response HEAD response from a server
  * @returns {Promise} Returns promise that fulfils into new Response object
+ * @fulfils {Response}
  * @private
  */
-function onHeadResponse(response) {
+function onHeadResponse(event, response) {
   const contentLength = response.headers.get('content-length');
-  const url = response.url;
 
   return Promise
-    .all(buildPartialRequests(url, contentLength))
+    .all(buildPartialRequests(event.request.clone(), contentLength))
     .then(responses => Promise.all(responses.map(res => res.arrayBuffer())))
     .then(buffers => new Response(buffers.reduce(concatArrayBuffer)));
 }
 
-// Registers all needed events in Service Worker
-self.addEventListener('install', event => console.log('[ServiceWorker] Installed'));
-self.addEventListener('activate', event => console.log('[ServiceWorker] Activated'));
-self.addEventListener('fetch', event => event.respondWith(fetch(event.request.url, {method: 'HEAD'}).then(onHeadResponse)));
+/**
+ * Triggers each time when fetch event fires in Service Worker
+ * @param {FetchEvent} event
+ * @returns {Promise}
+ * @fulfils {Response}
+ * @private
+ */
+function onFetch(event) {
+  //TODO: make request constructor more flexible
+  const request = new Request(event.request.url, {
+    method: 'HEAD',
+    headers: event.request.headers,
+    body: event.request.body,
+    mode: event.request.mode,
+    credentials: event.request.credentials,
+    cache: event.request.cache,
+    redirect: event.request.redirect,
+    referrer: event.request.referrer,
+    integrity: event.request.integrity
+  });
+
+  return event.respondWith(fetch(request).then(onHeadResponse.bind(this, event)));
+}
+
+self.addEventListener('fetch', onFetch);
