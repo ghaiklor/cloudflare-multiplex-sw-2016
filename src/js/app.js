9b9acce6bf6c42bb85b06c40c3f21d675c2f7118
Eugene Obrezkov <ghaiklor@gmail.com>
2016-08-24 10:53:47 +0300

    refactor: remove unneccessary warning when SW is not supports by browser

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,1 @@
-"use strict";
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js', {scope: '/'});
-} else {
-  console.warn(`Your browser doesn't support Service Workers`);
-}
+if ('serviceWorker' in navigator) navigator.serviceWorker.register('service-worker.js', {scope: '/'});
